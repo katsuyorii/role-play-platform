c8366a5f13c4f1a41dfe1aef417d352b1ff3c611
katsuyorii <katsuyoriiii@gmail.com>
2025-08-10 16:46:06 +0300

    Create schemas for auth and validator password

--- backend/auth/routers.py
+++ backend/auth/routers.py
@@ -1,6 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+
+from .services import AuthService
+from .dependencies import get_auth_service
+
 
 auth_router = APIRouter(
     prefix='/auth',
     tags=['Auth'],
 )
+
+@auth_router.post('/registration', status_code=status.HTTP_201_CREATED)
+async def registration_user(auth_service: AuthService = Depends(get_auth_service)):
+    pass
