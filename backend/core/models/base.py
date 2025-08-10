190cc09fc172d8092e1f4508e93517b5e4786570
katsuyorii <katsuyoriiii@gmail.com>
2025-08-10 14:08:47 +0300

    Add base model and include alembic

---
+++ backend/core/models/base.py
@@ -0,0 +1,14 @@
+import uuid
+
+from datetime import datetime, timezone
+
+from sqlalchemy import DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class BaseModel(DeclarativeBase):
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
