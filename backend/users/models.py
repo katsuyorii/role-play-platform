156a28447087f3428565f0dbaca45633cbd889de
katsuyorii <katsuyoriiii@gmail.com>
2025-08-13 05:21:04 +0300

    Change user model, add birthday, gender, last login fields

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,9 @@
-from sqlalchemy import String
+from sqlalchemy import String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 
 from enum import Enum
+
+from datetime import datetime, date
 
 from core.models.base import BaseModel
 
@@ -12,17 +14,26 @@ class UserRole(str, Enum):
     ADMIN = 'admin'
 
 
+class UserGender(str, Enum):
+    MALE = 'male'
+    FEMALE = 'female'
+
+
 class UserModel(BaseModel):
     __tablename__ = 'users'
     
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
     password: Mapped[str]
 
-    username: Mapped[str] = mapped_column(String(255), unique=True, nullable=True)
+    username: Mapped[str] = mapped_column(String(255), unique=True)
     role: Mapped[str] = mapped_column(String(5), default=UserRole.USER)
+    gender: Mapped[str] = mapped_column(String(6), nullable=True)
+    date_of_birth: Mapped[date] = mapped_column(nullable=True)
 
     # avatar_url
     # active_character
+
+    last_login: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
 
     is_active: Mapped[bool] = mapped_column(default=False)
     is_mailing: Mapped[bool] = mapped_column(default=True)
