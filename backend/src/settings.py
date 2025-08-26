e76c8626d9655ac7d9f28f384f357e11199a8445
None None <katsuyoriiii@gmail.com>
2025-08-26 05:40:05 +0300

    Create utils for create and verify jwt tokens and add jwt settings

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -17,4 +17,13 @@ class DatabaseSettings(CustomBaseSettings):
         return f'postgresql+asyncpg://{self.DB_USERNAME}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
 
 
+class JWTSettings(CustomBaseSettings):
+    JWT_PUBLIC_KEY: str
+    JWT_PRIVATE_KEY: str
+    JWT_ALGORITHM: str
+    JWT_ACCESS_TOKEN_MINUTES_EXPIRES: int
+    JWT_REFRESH_TOKEN_DAYS_EXPIRES: int
+
+
 database_settings = DatabaseSettings()
+jwt_settings = JWTSettings()
