a8da1280089405b180fafbfff874bfa3327cb494
None None <katsuyoriiii@gmail.com>
2025-09-18 11:11:49 +0300

    Add schemas for registartion and response

--- backend/auth/routers.py
+++ backend/auth/routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, status
 
 from .dependencies import get_auth_service
+from .schemas import UserCreatedResponse, UserRegistrationSchema
 from .services import AuthService
 
 
@@ -9,6 +10,6 @@ auth_router = APIRouter(
     tags=['Auth'],
 )
 
-@auth_router.post('/registration', status_code=status.HTTP_201_CREATED)
-async def registration_user(auth_service: AuthService = Depends(get_auth_service)):
+@auth_router.post('/registration', response_model=UserCreatedResponse, status_code=status.HTTP_201_CREATED)
+async def registration_user(user_data: UserRegistrationSchema, auth_service: AuthService = Depends(get_auth_service)):
     pass
---
+++ backend/auth/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr, Field, field_validator
+
+from core.validators.password import validator_password_complexity
+
+
+class UserCreatedResponse(BaseModel):
+    email: EmailStr
+
+
+class UserRegistrationSchema(BaseModel):
+    email: EmailStr = Field(max_length=255)
+    password: str
+
+    @field_validator('password')
+    @classmethod
+    def validate_password(cls, value):
+        return validator_password_complexity(value)
