6f2c6c37a9e8e9aa503cba45d2fb6f4d384c7ed4
None None <katsuyoriiii@gmail.com>
2025-09-18 12:32:47 +0300

    Include slowapi and create rate limiter

--- backend/auth/routers.py
+++ backend/auth/routers.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Request
 
+from src.limiter import limiter
 from .dependencies import get_auth_service
 from .schemas import UserCreatedResponse, UserRegistrationSchema
 from .services import AuthService
@@ -11,5 +12,6 @@ auth_router = APIRouter(
 )
 
 @auth_router.post('/registration', response_model=UserCreatedResponse, status_code=status.HTTP_201_CREATED)
-async def registration_user(user_data: UserRegistrationSchema, auth_service: AuthService = Depends(get_auth_service)):
+@limiter.limit('5/minute')
+async def registration_user(user_data: UserRegistrationSchema, request: Request, auth_service: AuthService = Depends(get_auth_service)):
     return await auth_service.registration(user_data)
